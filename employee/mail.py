aa3e0ac201380f10d715e625e40fb1abe266a649
ifeanyiokala <ifeanyi.okala.247238@unn.edu.ng>
2022-12-28 14:11:11 +0100

    endpoint

--- employee/mail.py
+++ employee/mail.py
@@ -13,13 +13,13 @@ def send(SUBJECT, TEXT):
     s.starttls()
 
     # Authentication
-    s.login("nsukkaai@gmail.com", "itisworthmylifetime")
+    s.login("ifeanyi.okala.247238@unn.edu.ng", PSWD)
 
     # message to be sent
     message = 'Subject: {}\n\n{}'.format(SUBJECT, TEXT)
 
     # sending the mail
-    s.sendmail("nsukkaai@gmail.com", "nsukkaai@gmail.com", message)
+    s.sendmail("ifeanyi.okala.247238@unn.edu.ng", "ifeanyi.okala.247238@unn.edu.ng", message)
 
     # terminating the session
     s.quit()
